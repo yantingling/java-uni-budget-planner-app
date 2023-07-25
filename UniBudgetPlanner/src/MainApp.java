25fbfc317849827e1eeaf120c98fc2a1159d64dc
Yan <104501044+yantingling@users.noreply.github.com>
2023-07-25 19:28:58 +0800

    Update MainApp.java

--- UniBudgetPlanner/src/MainApp.java
+++ UniBudgetPlanner/src/MainApp.java
@@ -1,3 +1,10 @@
+/*
+TMF2954 Java Programming (Project - UNI Budget Planner)
+Lecturer: Dr Tan Ping Ping
+Due Date: 26 June 2023
+Created by Ling Yan Ting
+*/
+
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import com.formdev.flatlaf.FlatLightLaf;
@@ -19,65 +26,3 @@ public class MainApp {
         new Splash();  
     }
 }
-
-/* Task contributions:
-
---------------------------------------------------------
-Ling Yan Ting (78115)
-
-1.	Created and implemented:
-    a.	Class MainApp
-    b.	Class Summary
-    c.	Class Graph
-    d.	Interface interfaceSummary
-    e.	Interface interfaceGraph
-
-2.	Tested and debug:
-    a.	Class AddTransaction
-    b.	Class EditExpenses
-    c.	Class EditIncome
-    d.	Interface interfaceTransactionOperations
-
---------------------------------------------------------
-Juwie Karen Christney (79725)
-
-1.	Created and implemented:
-    a.	Class Splash
-    b.	Class HomePage
-    c.	Interface interfaceAllRecords
-    d.	Interface interfaceTransactionOperations
-
-2.	Tested and debug:
-    a.	Class Summary
-    b.	Class EditExpenses
-    c.	Class EditIncome
-    d.	Interface interfaceSummary
-
---------------------------------------------------------
-Nik Nur Maiza Arisha Shamira binti Sakri (78319)
-
-1.	Created and implemented:
-    a.	Class Record
-    b.	Class AddTransaction
-    c.	Interface interfaceAllRecords
-    d.	Interface interfaceTransactionOperations
-
-2.	Tested and debug:
-    a.	Class Splash
-    b.	Class Graph
-    c.	Interface interfaceGraph
-
---------------------------------------------------------
-Marchell Dwayne Anak Damien (79963)
-
-1.	Created and implemented:
-    a.	Class EditExpenses
-    b.	Class EditIncome
-    c.	Interface interfaceAllRecords
-    d.	Interface interfaceTransactionOperations
-
-2.	Tested and debug:
-    a.	Class HomePage
-    b.	Class Record
-    c.	Class AddTransaction
-*/
