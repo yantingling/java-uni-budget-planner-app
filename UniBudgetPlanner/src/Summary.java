3da310f1349e09a56763c88a1e00ba8dd2d99319
Yan <104501044+yantingling@users.noreply.github.com>
2023-07-25 19:30:24 +0800

    Update Summary.java

--- UniBudgetPlanner/src/Summary.java
+++ UniBudgetPlanner/src/Summary.java
@@ -1,3 +1,10 @@
+/*
+TMF2954 Java Programming (Project - UNI Budget Planner)
+Lecturer: Dr Tan Ping Ping
+Due Date: 26 June 2023
+Created by Ling Yan Ting
+*/
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
